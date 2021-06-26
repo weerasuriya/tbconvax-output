a544e9e5d62c5c35479f2a6a228c5d6ca92517e3
Chathika Weerasuriya <chathika@gmail.com>
2021-06-26 22:39:53 +0100

    initial HM commit

---
+++ mktree.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p
+$1/{cm,current/{calibration/inc_split,flow/{BL,VX},state/{BL,VX}},metadata,params/{figs,adj_mort,seeds,sets},raw_output/traj}
+touch $1/{cm,current/{calibration/inc_split,flow/{BL,VX},state/{BL,VX}},metadata,params/{seeds,sets},raw_output/traj}/.mkfolder
