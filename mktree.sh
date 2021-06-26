41d78844681766268e0e65524eb7b748c3c53865
Chathika Weerasuriya <chathika@gmail.com>
2021-06-26 22:46:17 +0100

    M2-SF

--- mktree.sh
+++ mktree.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p $1/{cm,current/{calibration/inc_split,flow/{BL,VX},state/{BL,VX}},metadata,params/{figs,adj_mort,seeds,sets},raw_output/traj}
-touch $1/{cm,current/{calibration/inc_split,flow/{BL,VX},state/{BL,VX}},metadata,params/{seeds,sets},raw_output/traj}/.mkfolder
+touch $1/{cm,current/{calibration/inc_split,flow/{BL,VX},state/{BL,VX}},metadata,params/{figs,adj_mort,seeds,sets},raw_output/traj}/.mkfolder
